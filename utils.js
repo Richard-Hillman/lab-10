bd0a683aae6d3072b75e1b6bdfb196527d73a268
Richard Hillman <Richard.CC.Hillman@gmail.com>
2020-11-14 15:25:49 -0800

    yelp munge added

--- utils.js
+++ utils.js
@@ -19,8 +19,39 @@ function mungeWeather(location) {
 
 // ----------------------------------------------
 
+function mungeHiker(hike) {
+  let hikes = hike.trails.map((item) => {
+    return {
+      name: item.name,
+      location: item.location,
+      length: item.length,
+      stars: item.stars, 
+      star_votes: item.starVotes,
+      summary: item.summary,
+      trail_url: item.url,
+      conditions: item.conditionStatus,
+      condition_date: item.conditionDate.split(' ')[0],
+      condition_time: item.conditionDate.split(' ')[1],
+    };
+  });
 
+  return hikes.slice(0, 6);
+}
+
+// ----------------------------------------------
+
+function mungeYelp(location) {
+  return location.businesses.map(item => {
+    return {
+      name: item.name,
+      image_url: item.image_url,
+      price: item.price,
+      rating: item.rating,
+      url: item.url
+    };
+  }).slice(0, 20);
+}
 
 module.exports = {
-  mungeLocation, mungeWeather
+  mungeLocation, mungeWeather, mungeHiker, mungeYelp
 };
